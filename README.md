b2c2d727cd5642edc8e391d88541c9e4b5996611
Adrian Silva <adrian@ageda.net>
2012-06-27 16:09:27 +0200

    markdown readme as per FC011

---
+++ README.md
@@ -0,0 +1,4 @@
+DESCRIPTION
+===========
+
+manage node local hostname
--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@ maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Ensures hostname is set correctly"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.1"
 
 %w{ ubuntu }.each do |os|
   supports os
